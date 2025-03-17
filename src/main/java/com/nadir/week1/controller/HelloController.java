5ae92ed19bbc776ce353319e7e5a6e445d5dd28d
lafleurdev <lafleur.dev1@gmail.com>
2025-03-18 02:15:34 +0300

    Add HelloController

---
+++ src/main/java/com/nadir/week1/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.nadir.week1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "SD-week1, Hello from my first Spring Boot controller!";
+    }
+}
